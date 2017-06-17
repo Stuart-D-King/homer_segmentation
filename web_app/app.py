43d8f80d26ce86b853bd786bf1a2a0839e41a255
Stuart King <stuartking@Stuarts-MacBook.local>
2017-06-17 13:41:34 -0600

    port update to app

--- web_app/app.py
+++ web_app/app.py
@@ -160,4 +160,4 @@ def show_marker_map():
     return send_file('static/img/marker_cluster.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=8105, debug=True, threaded=True)
