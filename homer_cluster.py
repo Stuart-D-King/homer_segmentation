efe362aadf822547cb64552fafa3c6c1beaf721d
Stuart King <stuartking@Stuarts-MacBook.local>
2017-06-20 10:10:26 -0600

    updated cluster script

--- homer_cluster.py
+++ homer_cluster.py
@@ -13,6 +13,12 @@ import pickle
 import pdb
 
 def one_hot(df_):
+    '''
+    Convert dataframe into a binary array using sklearn's one-hot encoding processing package.
+
+    :param df_: dataframe for which one-hot encoding is to be performed
+    :returns: encoded numpy array
+    '''
     df = df_.copy()
 
     cols_to_keep = ['UserRole',
@@ -48,21 +54,45 @@ def one_hot(df_):
     return X
 
 def run_mca(X):
+    '''
+    Perform Multiple Correspondence Analysis (MCA) on input array.
+
+    :param X: array for which MCA is to be performed
+    :returns: MCA instance and transformed array
+    '''
     mca_ben = mca.MCA(X)
     X = mca_ben.fs_r(1)
     return mca_ben, X
 
 def run_tsne(X):
+    '''
+    Perform TSNE on input array.
+
+    :param X: array for which TSNE is to be performed
+    :returns: transformed array
+    '''
     model = TSNE(n_components=6, method='exact', random_state=0)
     X = model.fit_transform(X)
     return X
 
 def run_pca(X):
+    '''
+    Perform Principle Component Analysis (PCA) on input array
+
+    :param X: array for which PCA is to be performed
+    :returns: PCA instance and transformed array
+    '''
     pca = PCA(n_components=8, random_state=42)
     X_transformed = pca.fit_transform(X)
     return pca, X_transformed
 
 def prep_kproto(df_):
+    '''
+    Prepare input dataframe for k-prototypes clustering.
+
+    :param df_: dataframe to be prepared
+    :returns: list of index position of categorical variables, prepared dataframe
+    '''
     df = df_.copy()
 
     cols_to_keep = ['NumSims',
@@ -101,6 +131,12 @@ def prep_kproto(df_):
     return cat_cols, df
 
 def prep_kmodes(df_):
+    '''
+    Prepare input dataframe for k-modes clustering.
+
+    :param df_: dataframe to be prepared
+    :returns: prepared dataframe
+    '''
     df = df_.copy()
 
     cols_to_keep = ['UserRole',
@@ -128,16 +164,41 @@ def prep_kmodes(df_):
     return df
 
 def run_kproto(X, cat_cols, init_method='Cao', n_clusters=4):
+    '''
+    Perform k-prototypes clustering.
+
+    :param X: prepared array for clustering
+    :param cat_cols: list of index positions for categorical variables
+    :param init_method: initiation method for k-prototypes clustering, default = 'Cao'
+    :param n_clusters: number of clusters for model to segment data, default = 4
+    :returns: k-prototypes models, array of labels
+    '''
     kp = kprototypes.KPrototypes(n_clusters=n_clusters, init=init_method, n_init=10, max_iter=5, verbose=2)
     labels = kp.fit_predict(X, categorical=cat_cols)
     return kp, labels
 
 def run_kmodes(X, init_method='Huang', n_clusters=4):
+    '''
+    Perform k-modes clustering.
+
+    :param X: prepared array for clustering
+    :param init_method: initiation method for k-prototypes clustering, default = 'Huang'
+    :param n_clusters: number of clusters for model to segment data, default = 4
+    :returns: k-modes models, array of labels
+    '''
     km = kmodes.KModes(n_clusters=n_clusters, n_init=10, init=init_method, verbose=1)
     labels = km.fit_predict(X)
     return km, labels
 
 def plot_clusters(X, labels, n_clusters=4):
+    '''
+    Plot a 2-D representation of clusters.
+
+    :param X: dimensionaly reduced feature space (i.e. array post-PCA)
+    :param labels: array of labels for X
+    :param n_clusters: number used to cluster input array X
+    :returns: None, saved plot to 'img' folder
+    '''
     fig = plt.figure(figsize=(8, 6))
     ax = fig.add_subplot(111)
 
@@ -165,12 +226,6 @@ if __name__ == '__main__':
     # df_users['Cluster'] = df_users['Cluster'].apply(lambda x: x+1)
     df['Cluster'] = km_labels
     df['Cluster'] = df['Cluster'].apply(lambda x: x+1)
-
-    # ---Map cluster labels to full dataframe---
-    # keys = df_users['UserId'].tolist()
-    # values = df_users['Cluster'].tolist()
-    # user_cluster_dct = dict(zip(keys, values))
-    # df['Cluster'] = df['User'].map(user_cluster_dct)
 
     # ---Pickle clustered dataframes and KModes model---
     # df_users.to_pickle('data/df_users_clustered.pkl')
